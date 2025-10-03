fb122725780517f95f95ea29c9ddb8ca0e220228
Meryemsn01 <meryemsouaini01@gmail.com>
2025-10-03 16:32:43 +0100

    feat(ui): Build frontend for invitation flow
    Adds the 'Invite Freelancer' modal to the project board. Creates the public 'Accept Invitation' page for new freelancers to register.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import DashboardPage from './pages/DashboardPage';
 import WorkspaceDetailPage from './pages/WorkspaceDetailPage';
 import PrivateRoute from './components/PrivateRoute';
 import ProjectBoardPage from './pages/ProjectBoardPage';
+import AcceptInvitationPage from './pages/AcceptInvitationPage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/accept-invitation" element={<AcceptInvitationPage />} />
           
 
           <Route element={<PrivateRoute />}>
